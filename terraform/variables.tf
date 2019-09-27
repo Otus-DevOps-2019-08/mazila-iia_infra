f865e8ae9b389e0894d20f130d974befdaa04f15
Ivan Ageychev <siniygus@gmail.com>
2019-09-27 08:01:37 +0000

    create terraform project

---
+++ terraform/variables.tf
@@ -0,0 +1,25 @@
+variable project {
+  description = "Project ID"
+}
+variable region {
+  description = "Region"
+  # Значение по умолчанию
+  default = "europe-west1"
+}
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+
+variable private_key_path {
+  # Описание переменной
+  description = "Path to the privat key"
+}
+
+variable zone {
+  default = "europe-west1-b"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
